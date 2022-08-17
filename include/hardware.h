fa7cd0006c9184e80f123b37b8a9b2897184d12f
deadbok <martin.groenholdt@gmail.com>
2022-08-17 11:02:22 +0100

    Added buttons and led defines

--- include/hardware.h
+++ include/hardware.h
@@ -11,13 +11,21 @@
 #ifndef _HARDWARE_H_
 #define _HARDWARE_H_
 
-// DS1302 RTC
-#define DS1302_SCLK  16
-#define DS1302_CE    05
-#define DS1302_IO    14
+//DS1302 RTC
+#define DS1302_SCLK     16
+#define DS1302_CE       05
+#define DS1302_IO       14
 
-// TM1650 display driver
-#define TM1650_SCL 12
-#define TM1650_SDA 13
+//TM1650 display driver
+#define TM1650_SCL      12
+#define TM1650_SDA      13
+
+//LED Next to the esp8266 on the board
+#define LED_PIN         2
+
+//The buttons on the board
+#define BUTTON_1        1
+#define BUTTON_2        2
+#define BUTTON_3        4
 
 #endif

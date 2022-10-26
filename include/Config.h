c90b026f1176cbd088545102b05602bd8c5ddb26
deadbok <martin.groenholdt@gmail.com>
2022-10-26 21:08:07 +0200

    Added basic config class

---
+++ include/Config.h
@@ -0,0 +1,53 @@
+/**
+ * NTP LED Clock.
+ * 
+ * config.h
+ *
+ * Class to hold and manage config values the clock.
+ *
+ * Copyright 2022 Martin Bo Kristensen Grønholdt
+ */
+#include <WString.h>
+
+#ifndef config_h
+#define config_h
+
+class Config
+{
+    public:
+        Config();
+
+        void setNTPServer(String server);
+        String getNTPServer();
+
+        void setTimeZone(String TZ);
+        String getTimeZone();
+
+        void setTime(String time);
+        String getTime();
+
+        void setBrightness(unsigned char brightness);
+        unsigned char getBrightness();
+
+        void setSSID(String SSID);
+        String getSSID();
+
+        void setHostname(String hostname);
+        String getHostname();
+
+        ~Config();
+    private:
+        //Version of the configuration data in the class.
+        const unsigned char vmajor = 0;
+        const unsigned char vminor = 1;
+
+        //Actual confiuration data.
+        String              NTPServer;
+        String	            timeZone;
+        String			    time;
+		unsigned char       brightness;
+        String			    SSID;
+		String              hostname;
+};
+
+#endif
---
+++ src/Config.cpp
@@ -0,0 +1,71 @@
+#include <Esp.h>
+#include "Config.h"
+
+Config::Config() : NTPServer(""), timeZone(""), time(""), brightness(4),
+                   SSID(""), hostname("clock" + ESP.getChipId())
+{
+}
+
+void Config::setNTPServer(String server)
+{
+    this->NTPServer = server;
+}
+
+String Config::getNTPServer()
+{
+    return(this->NTPServer);
+}
+
+void Config::setTimeZone(String TZ)
+{
+    this->timeZone = TZ;
+}
+
+String Config::getTimeZone()
+{
+    return(this->timeZone);
+}
+
+void Config::setTime(String time)
+{
+    this->time = time;
+}
+
+String Config::getTime()
+{
+    return(this->time);
+}
+
+void Config::setBrightness(unsigned char brightness)
+{
+    this->brightness = brightness;
+}
+
+unsigned char Config::getBrightness()
+{
+    return(this->brightness);
+}
+
+void Config::setSSID(String SSID)
+{
+    this->SSID = SSID;
+}
+
+String Config::getSSID()
+{
+    return(this->SSID);
+}
+
+void Config::setHostname(String hostname)
+{
+    this->hostname = hostname;
+}
+
+String Config::getHostname()
+{
+    return(this->hostname);
+}
+
+Config::~Config()
+{
+}
